1213fbb94e306c899624d62f49fbc70a11a9c2a1
RudyR1 <rudy.rano972@gmail.com>
2023-04-03 12:14:44 +0200

    react_basics_6_state

--- src/App.jsx
+++ src/App.jsx
@@ -7,49 +7,52 @@ import PokemonCard from './components/PokemonCard'
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonIndex, setPokemonIndex] = useState(2)
 
+  const ShowPrevious = () => {
+    setPokemonIndex ( pokemonIndex -1);
+  };
+
+  const NextPrevious = () => {
+    setPokemonIndex ( pokemonIndex +1);
+  };
   return (
     <div className="App">
-      <div>
-      <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav>
+        {pokemonIndex > 0 && ( <button type='button' onClick={ShowPrevious}>Previous</button>)}
+        
+        {pokemonIndex < pokemonList.length-1 && (<button type='button' onClick={NextPrevious}>Next</button>)}
+
+      </nav>
+
+          <PokemonCard pokemon={pokemonList[pokemonIndex]} />
         
         
-      
-      </div>
-      
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-        <div>
-          <PokemonCard pokemon={pokemonList[0]} />
-        </div>
-    
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      
     </div>
   )
   
